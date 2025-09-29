a40aaeecae419dac55bd83d1f35290c1c49b80d9
João <jsarracine@gmail.com>
2025-09-29 11:32:59 -0300

    Declarando variáveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
-alert("Hello World!")
+// alert("Hello World!")
+
+// DECLARAÇÃO E VARIÁVEIS
+// existem 3 tipos de variação 
+
+var nome = "Fiap teste";
+// declaração // variável // valor da variável 
+console.log(typeof nome);
+
+let nome1 = 10;
+console.log(typeof nome1);
+
+// {} objeto / lista - objeto (atributos ao objeto e seus valores)
+// [] array  / lista - array (recebe um monte de objetos)
+// () parâmetros / metodos 
+const nome2 = {};
+console.log(typeof nome2);
+
+let nome3 = [];
+console.log(typeof nome3);
+
+let nome5=true;
+console.log(typeof nome5);
